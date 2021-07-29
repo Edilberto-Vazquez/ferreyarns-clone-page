10eb1c80dd35375bcda0f0e9ed8667c007c9a47d
Edilberto <edi_vazquez12@hotmail.com>
2021-07-28 21:20:35 -0500

    StorySlides css

--- src/components/StorySlides.jsx
+++ src/components/StorySlides.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import useOpacityAnm from "../utils/animations/useOpacityAnm";
+import "./styles/Storyslide.css";
 
 const StorySlides = ({ img, paragraph, title, yearImg }) => {
   const [refOpc] = useOpacityAnm();
